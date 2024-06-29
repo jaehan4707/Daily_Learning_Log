dd43d79c3074f264b1c967f443fa368100cd3d75
jaehan4707 <jahan4707@naver.com>
2024-06-29 13:33:35 +0900

    [Algorithm] - 12738 가장 긴 증가하는 부분 수열3(Kotlin)[골드2]

---
+++ Algorithm/Boj/Kotlin/[백준 12738]/Solution.kt
@@ -0,0 +1,50 @@
+class Solution {
+
+    private val br = System.`in`.bufferedReader()
+    private var n = 0
+    private lateinit var graph: LongArray
+    private var answer = arrayListOf<Long>()
+
+
+    fun run() {
+        input()
+        solution()
+        print(answer.size)
+    }
+
+    private fun input() {
+        n = br.readLine().toInt()
+        graph = br.readLine().split(" ").map { it.toLong() }.toLongArray()
+    }
+
+    private fun solution() {
+        for (i in graph.indices) {
+            if (answer.size == 0 || answer.last() < graph[i]) {
+                answer.add(graph[i])
+            } else {
+                val idx = search(graph[i])
+                answer[idx] = graph[i]
+            }
+        }
+    }
+
+    private fun search(number: Long): Int {
+        var left = 0
+        var right = answer.size
+        while (left <= right) {
+            val mid = (left + right)/ 2
+            if (answer[mid] >= number) { // number가 더 작으면
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        return left
+    }
+
+
+}
+
+fun main() {
+    Solution().run()
+}
